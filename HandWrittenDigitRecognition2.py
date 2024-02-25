45fca1dcce5e9846bf90dd2dbe4b430732183a82
Seyed Ali Pourheidar <135211476+apourheidar@users.noreply.github.com>
2024-02-25 19:06:30 +0330

    Add files via upload

---
+++ HandWrittenDigitRecognition2.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+
+def draw(n):
+    plt.imshow(n, cmap=plt.cm.binary)
+    plt.show()
+
+
+draw(x_train[0])
+print("x_train[0]")
+print(x_train[0])
+print("x_train[0].shape")
+
+print(x_train[0].shape)
+
+
+# there are two types of models
+# sequential is most common, why?
+
+model = tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# reshape
+
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy']
+              )
+model.fit(x_train, y_train, epochs=3)
+
+val_loss,val_acc = model.evaluate(x_test,y_test)
+print("loss-> ",val_loss,"\nacc-> ",val_acc)
+
+predictions = model.predict([x_test])
+print('label -> ', y_test[2])
+print('prediction -> ', np.argmax(predictions[2]))
+
+draw(x_test[2])
+
+# saving the model
+# .h5 or .model can be used
+
+model.save('epic_num_reader.h5')
+
+#new_model = tf.keras.models.load_model('epic_num_reader.h5')
+
+#predictions = new_model.predict([x_test])
+
+#print('label -> ', y_test[2])
+#print('prediction -> ', np.argmax(predictions[2]))
+
+#draw(x_test[2])
